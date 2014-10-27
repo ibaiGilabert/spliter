b5b69000fdb4cba5bb465f5d3ca23cd7787b2978
Ibai <ibai.gilabert@gmail.com>
2014-10-27 17:41:17 +0100

    word counter

--- fork.cpp
+++ fork.cpp
@@ -2,11 +2,19 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#include <fstream>
 #include <sstream>
 #include <iostream>
 #include <string>
+#include <vector>
+
+#include <boost/algorithm/string.hpp>
 
 using namespace std;
+
+void usage() {
+	fprintf(stderr, "Input param. required\n"); exit(1);
+}
 
 string exec(const char* cmd) {
     FILE* pipe = popen(cmd, "r");
@@ -21,16 +29,65 @@ string exec(const char* cmd) {
     return result;
 }
 
-int main(int argc, char *argv[]) {
-	printf("[FATHER]: My ID: %d\n", (int)getpid());
-	string job = "JOB=/usr/local/sge/bin/linux-x64/qsub spliter/run.sh a";
-	printf("Let's invoque spliter\n");
-	string job_i = exec(job.c_str());
-	string job_id = exec("echo $JOB");
-	/*stringstream st;
-	st << system(job_id.c_str());
-	string st_s = st.str();
-	printf("job_id: %s\n", st_s.c_str())*/;
-	//printf("OUTPUT: %s\n", job_i.c_str());
-	printf("EXECUTED /jobid: %s\n", job_id.c_str());
+bool end(string id) {
+	// check whether id job is already ended
+	bool ended = true;
+        string out = exec("qstat");
+        istringstream bufo(out);
+        int i = 0;
+        for (string token; getline(bufo, token, '\n'); ++i) {
+                string job_aux;
+                istringstream aux(token);
+                getline(aux, job_aux, ' ');
+
+		if (job_aux == id) ended = false;
+                /*if (atoi(job_aux.c_str()) > 0)
+                        cout << "job_ID: " << atoi(job_aux.c_str()) << endl;*/
+        }
+	return ended;
 }
+
+string getJobID(string cmd) {
+	// get the id job from qsub's return value
+        boost::algorithm::trim(cmd);
+        //printf("return qsub: |%s|\n", job.c_str());
+        vector<string> v;
+        istringstream buf(cmd);
+        for (string token; getline(buf, token, ' '); ) v.push_back(token);
+        /*for (int j = 0; j < v.size(); ++j) cout << "v[" << j << "]: " << v[j] << "\t";
+        cout << endl;*/
+	return v[2];
+}
+
+int main(int argc, char *argv[]) {
+	if (argc < 2) usage();
+
+	int c = 0;
+	string line;
+	ifstream file(argv[1]);
+	if (file) {
+		while(getline(file, line)) {
+			istringstream bufer(line);
+			for (string token; getline(bufer, token, ' '); ) c++;
+		}
+		file.close();
+	} else { fprintf(stderr, "couldn't open file: %s\n", argv[1]); exit(1); }
+
+	printf("--> %s file has %d words\n", argv[1], c);
+
+/*	printf("[FATHER]: My ID: %d\n", (int)getpid());
+	printf("[FATHER]: jobID: %s\n", exec("echo $JOB_ID").c_str());
+        printf("Let's invoque spliter\n");
+
+	string cmd1 = exec("/usr/local/sge/bin/linux-x64/qsub /home/usuaris/gilabert/spliter/countdown a");
+	string jobid1 = getJobID(cmd1);
+	printf("EXECUTED /jobID1: %s\n", jobid1.c_str());
+
+        string cmd2 = exec(string("/usr/local/sge/bin/linux-x64/qsub -hold_jid "+ jobid1 +" /home/usuaris/gilabert/spliter/countdown a").c_str());
+        string jobid2 = getJobID(cmd2);
+        printf("EXECUTED /jobID2: %s (-hold_jid <%s>)\n", jobid2.c_str(), jobid1.c_str());
+
+	while (!end(jobid2)) {}
+
+	cout << "DONE" << endl;
+*/}
